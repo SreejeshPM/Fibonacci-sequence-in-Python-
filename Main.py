785bef33587462ffe2c74d148f5ef726f234fd33
SreejeshPM <sreejeshpmohanan@gmail.com>
2018-07-06 16:37:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 # Fibonacci-sequence-in-Python
 # Creating the most easy way of creating Fibonacci sequence in python less than 10 lines of code
-NumberofLetters = []
+FibanocciSeries = []
 k = 10  #Number of sequence you want
 for item in range(k):
-    if len(NumberofLetters) >= 2:
-        NumberofLetters.append(NumberofLetters[-1]+NumberofLetters[-2])
+    if len(FibanocciSeries) >= 2:
+        FibanocciSeries.append(FibanocciSeries[-1]+FibanocciSeries[-2])
     else:
-        NumberofLetters = [0,1]
-        
+        FibanocciSeries = [0,1]
+ """
+ Now Print FibanocciSeries
+ """
  
